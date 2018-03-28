5e50390b4dfd4d53d8f0155c4c9b8789d8cb5f3c
rksazid <rksazid@gmail.com>
2018-03-28 21:33:27 +0600

    commit by rksazid

--- app/src/main/java/com/bidtokroy/fireproject/MainActivity.java
+++ app/src/main/java/com/bidtokroy/fireproject/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //This is rksazid
+        //ok done
+        //eibar khela hobe :D
     }
 }
